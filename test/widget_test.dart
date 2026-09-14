f0ce121940f2745ea186f6644d000ed128e7915b
HarshaPriyankara <h9pharsha@gmail.com>
2026-09-14 22:09:12 +0530

    first example commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,9 @@ import 'package:simpledo_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+    await tester.pumpWidget(const TodoApp());
+    
+    expect(find.text('My To-Do List'), findsOneWidget);
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
